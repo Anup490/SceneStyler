a8441d3a932ca19418021fe52b457ddbd2133548
Anup490 <anupbhandari490@gmail.com>
2022-11-01 12:29:29 +0530

    Added drag constraints

--- Assets/Behaviours/FloorMovementBehaviour.cs
+++ Assets/Behaviours/FloorMovementBehaviour.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class FloorMovementBehaviour : AssetBehaviour
 {
+    const float minx = -3.0f;
+    const float maxx = 3.0f;
+    const float minz = -8.0f;
+    const float maxz = -4.5f;
+
     public override void Displace(Vector3 diff)
-    {
-        Transform transform = GetComponent<Transform>();
+    {    
         Vector3 actualDiff = new Vector3(diff.x, 0.0f, diff.y);
-        transform.position += actualDiff;
+        Vector3 newPos = transform.position + actualDiff;
+        if (IsWithinBounds(newPos))
+            transform.position = newPos;
+    }
+
+    bool IsWithinBounds(Vector3 newPos)
+    {
+        float zdiff = transform.position.z - minz;
+        float actualMinx = minx - zdiff;
+        float actualMaxx = maxx + zdiff;
+        return newPos.x > actualMinx && newPos.x < actualMaxx && newPos.z < maxz;
     }
 }
--- Assets/Behaviours/WallMovementBehaviour.cs
+++ Assets/Behaviours/WallMovementBehaviour.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class WallMovementBehaviour : AssetBehaviour
 {
+    const float minx = -9.0f;
+    const float maxx = 9.0f;
+    const float miny = 0.5f;
+    const float maxy = 4.5f;
+
     public override void Displace(Vector3 diff)
     {
-        Transform transform = GetComponent<Transform>();
         Vector3 actualDiff = new Vector3(diff.x, diff.y, 0.0f);
-        transform.position += actualDiff;
+        Vector3 newPos = transform.position + actualDiff;
+        if (IsWithinBounds(newPos))
+            transform.position = newPos;
+    }
+
+    bool IsWithinBounds(Vector3 newPos)
+    {
+        return newPos.x > minx && newPos.x < maxx && newPos.y > miny && newPos.y < maxy;
     }
 }
